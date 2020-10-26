9a3aef2ad3bd26b5f3c5967766a9dbc720da84da
Camilo <camilo.pulgarin11@gmail.com>
2020-10-25 23:58:23 -0500

    point 4 advanced

---
+++ 106-bitonic_sort.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+/**
+ * bitonic_sort - sort
+ * @array: Array
+ * @size: size
+ * Return: void
+ */
+void bitonic_sort(int *array, size_t size)
+{
+	int a = 1, b = 2;
+
+	if (a == 2 || b == 1 || !array || size < 2)
+		return;	
+}
